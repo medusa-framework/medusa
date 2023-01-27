7742fcbc9fca38ff9965434d610db0f8c375677a
justinelinette <justinelinetteneveu@gmail.com>
2023-01-27 00:40:16 +0000

    ACC-11 [IMP] REST Specific Endpoints

--- astro/user/routes/user.py
+++ astro/user/routes/user.py
@@ -4,7 +4,7 @@ from astro.user.controllers.user import UserController
 user1 = Blueprint("user", __name__)
 
 
-@user1.route("/user/create")
+@user1.route("/user/create", methods=["POST"])
 def create():
     return UserController().create()
 
@@ -19,21 +19,21 @@ def get():
     return UserController().get()
 
 
-@user1.route("/user/delete")
-def delete():
-    return UserController().delete()
-
-
-@user1.route("/user/delete/all")
+@user1.route("/user/delete/all", methods=["DELETE"])
 def delete_all():
     return UserController().delete_all()
 
 
-@user1.route("/user/update/all")
+@user1.route("/user/delete", methods=["DELETE"])
+def delete():
+    return UserController().delete()
+
+
+@user1.route("/user/update/all", methods=["PATCH"])
 def update():
     return UserController().update_all()
 
 
-@user1.route("/user/update")
+@user1.route("/user/update", methods=["PATCH"])
 def update_all():
     return UserController().update()
