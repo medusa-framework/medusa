77554e6f3883e06999b519c90c1eaab29f1a8497
justinelinette <justinelinetteneveu@gmail.com>
2023-01-28 00:35:51 +0000

    ACC-22 [IMP] Add URL Prefix to Blueprint

--- astro/movie/routes/movie.py
+++ astro/movie/routes/movie.py
@@ -4,36 +4,36 @@ from astro.movie.controllers.movie import MovieController
 movie = Blueprint("movie", __name__)
 
 
-@movie.route("/movie/create", methods=["POST"])
+@movie.route("/create", methods=["POST"])
 def create():
     return MovieController().create()
 
 
-@movie.route("/movie/get/all", methods=["GET"])
+@movie.route("/get/all", methods=["GET"])
 def get_all():
     return MovieController().get_all()
 
 
-@movie.route("/movie/get", methods=["GET"])
+@movie.route("/get", methods=["GET"])
 def get():
     return MovieController().get()
 
 
-@movie.route("/movie/delete/all", methods=["DELETE"])
+@movie.route("/delete/all", methods=["DELETE"])
 def delete_all():
     return MovieController().delete_all()
 
 
-@movie.route("/movie/delete", methods=["DELETE"])
+@movie.route("/delete", methods=["DELETE"])
 def delete():
     return MovieController().delete()
 
 
-@movie.route("/movie/update/all", methods=["PATCH"])
+@movie.route("/update/all", methods=["PATCH"])
 def update():
     return MovieController().update_all()
 
 
-@movie.route("/movie/update", methods=["PATCH"])
+@movie.route("/update", methods=["PATCH"])
 def update_all():
     return MovieController().update()
--- astro/user/routes/user.py
+++ astro/user/routes/user.py
@@ -4,36 +4,36 @@ from astro.user.controllers.user import UserController
 user1 = Blueprint("user", __name__)
 
 
-@user1.route("/user/create", methods=["POST"])
+@user1.route("/create", methods=["POST"])
 def create():
     return UserController().create()
 
 
-@user1.route("/user/get/all", methods=["GET"])
+@user1.route("/get/all", methods=["GET"])
 def get_all():
     return UserController().get_all()
 
 
-@user1.route("/user/get", methods=["GET"])
+@user1.route("/get", methods=["GET"])
 def get():
     return UserController().get()
 
 
-@user1.route("/user/delete/all", methods=["DELETE"])
+@user1.route("/delete/all", methods=["DELETE"])
 def delete_all():
     return UserController().delete_all()
 
 
-@user1.route("/user/delete", methods=["DELETE"])
+@user1.route("/delete", methods=["DELETE"])
 def delete():
     return UserController().delete()
 
 
-@user1.route("/user/update/all", methods=["PATCH"])
+@user1.route("/update/all", methods=["PATCH"])
 def update():
     return UserController().update_all()
 
 
-@user1.route("/user/update", methods=["PATCH"])
+@user1.route("/update", methods=["PATCH"])
 def update_all():
     return UserController().update()
