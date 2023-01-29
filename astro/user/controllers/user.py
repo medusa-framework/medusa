620c64be801bcedb9b719cad52e6b9faf7d3dcd4
justinelinette <justinelinetteneveu@gmail.com>
2023-01-29 04:32:35 +0000

    ACC-40 [FIX] Removed Extra UserController Function

--- astro/user/controllers/user.py
+++ astro/user/controllers/user.py
@@ -2,36 +2,4 @@ from astro.user.models.user import User
 
 
 class UserController:
-
-    def create(self):
-        return User().create(
-            username="georgegeorgegeorgegeorgegeorgegeorge",
-            password="george2",
-            email="george3"
-        )
-
-    def get_all(self):
-        return User().get_all()
-
-    def get(self):
-        return User().get()
-
-    def delete_all(self):
-        return User().delete_all()
-
-    def delete(self):
-        return User().delete()
-
-    def update_all(self):
-        return User().update_all(
-            username="john",
-            password="john2",
-            email="john3"
-        )
-
-    def update(self):
-        return User().update(
-            username="paul",
-            password="paul2",
-            email="paul3"
-        )
+    pass
